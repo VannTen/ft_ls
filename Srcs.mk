018c418b11c95339945af1f20b2b0ad7bcf09ad6
Max Gautier <mgautier@student.42.fr>
2017-03-29 15:18:06 +0200

    Initial commit
    Add Makefile, Rules.mk, Srcs.mk

---
+++ Srcs.mk
@@ -0,0 +1,33 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Srcs.mk                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2016/12/19 07:58:53 by mgautier          #+#    #+#              #
+#*   Updated: 2017/03/29 15:18:00 by mgautier         ###   ########.fr       *#
+#                                                                              #
+# **************************************************************************** #
+
+# Define target and sources
+
+TARGET := ft_ls
+
+# Directories
+
+SRC_DIR :=
+OBJ_DIR := object
+INC_DIR := includes
+DEP_DIR := .dep
+TEST_DIR := test
+
+# Dependencies
+
+LIBRARY := 
+OBJECTS :=
+ELSE :=
+
+# Sub directories
+
+SUBDIRS :=
