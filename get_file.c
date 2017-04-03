4aa7b581522f65594fcc9bade714dedf2d32cca5
Max Gautier <mgautier@student.42.fr>
2017-04-03 18:27:36 +0200

    Fix uncorrect behavior of get_stat_dir
    Offset on pathlen was causing uncorrect path

--- get_file.c
+++ get_file.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/31 17:12:04 by mgautier          #+#    #+#             */
-/*   Updated: 2017/04/03 17:37:23 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/04/03 18:25:57 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,28 @@
 #include <sys/types.h>
 #include <sys/dir.h>
 
-void	*get_dir_entry(DIR *dir)
+void	*get_dir_entry(DIR *dir, char *parent_path, int path_len)
 {
+	(void)parent_path;
+	(void)path_len;
 	return (readdir(dir));
 }
 
 void	*get_stat_dir(DIR *dir, char *parent_path, int path_len)
 {
 	struct s_file	*file;
+	struct dirent	*dir_entry;
 
+	dir_entry = readdir(dir);
+	if (dir_entry == NULL)
+		return (NULL);
 	file = ft_memalloc(sizeof(struct s_file));
-	file->dir_entry = readdir(dir);
+	file->dir_entry = dir_entry;
 	parent_path[path_len] = '/';
+	path_len++;
 	ft_strcpy(parent_path + path_len, file->dir_entry->d_name);
 	stat(parent_path, &file->file_infos);
+	path_len--;
 	ft_strclr(parent_path + path_len);
 	return (file);
 }
