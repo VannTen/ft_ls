aeb16c12a0d8a126a34ac2bc244071959ea1bbb9
Max Gautier <mgautier@student.42.fr>
2017-04-13 17:22:03 +0200

    Fix destroyers

--- destroy_file.c
+++ destroy_file.c
@@ -6,22 +6,19 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/07 20:01:32 by mgautier          #+#    #+#             */
-/*   Updated: 2017/04/07 20:06:16 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/04/13 13:20:11 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "file_defs.h"
-
-void	dir_entry_destroy(void **direntry)
-{
-	*(struct dirent**)direntry = NULL;
-}
+#include "libft.h"
 
 void	stat_entry_destroy(void **s_file)
 {
 	struct s_file	*file;
 
 	file = *s_file;
+	ft_strdel((char**)&file->dir_entry);
 	ft_bzero(file, sizeof(struct s_file));
 	free(file);
 	*s_file = NULL;
