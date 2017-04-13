b8a303498684279504bb86b8b1e2fafabb96afe7
Max Gautier <mgautier@student.42.fr>
2017-04-13 17:23:23 +0200

    Add is_link

--- is_file.c
+++ is_file.c
@@ -6,22 +6,30 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 13:28:09 by mgautier          #+#    #+#             */
-/*   Updated: 2017/04/11 16:00:55 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/04/13 11:56:27 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "file_defs.h"
 #include "mode_and_perms_interface.h"
 
-t_bool	is_dir(struct s_file *file)
+t_bool	is_dir(const struct s_file *file)
 {
 	return (file_type(file->file_infos.st_mode) == 'd');
 }
 
-t_bool	is_block_or_char_file(struct s_file *file)
+t_bool	is_block_or_char_file(const struct s_file *file)
 {
 	char type;
 
 	type = file_type(file->file_infos.st_mode);
 	return (type == 'b' || type == 'c');
+}
+
+t_bool	is_link(const struct s_file *file)
+{
+	char type;
+
+	type = file_type(file->file_infos.st_mode);
+	return (type == 'l');
 }
